f756d77a37940953e7bdbf17e6fdec3d64edf902
Gavin Kistner <!@phrogz.net>
2010-03-25 14:41:36 -0600

    Defense mode

--- js/board.js
+++ js/board.js
@@ -3,12 +3,26 @@ function Board(maxM,maxN,canvas,tiles){
 	this.maxN = maxN;
 	this.features = [];
 	this.path = [];
-	this.holes = [];
 	this.tiles = tiles || {};
 	this.canvas = canvas;
 	this.context = canvas.getContext('2d');
+	this.holes = [];
 	for (var m=0;m<=this.maxM;++m) this.holes[m] = [];
 };
+Board.prototype.placeHole = function(cell){
+	if (!this.inBounds(cell)) return;
+	if (cell.sameAs(this.start) || cell.sameAs(this.finish)) return;
+	if (!this.holes[cell.m]){
+		console.log( 'NO HOLE: %d', cell.m );
+		this.holes[cell.m]=[];
+	}
+	if (this.holes[cell.m][cell.n]) return;
+	this.holes[cell.m][cell.n] = true;
+	cell.tile = 'black';
+	this.features.push(cell);
+	this.redraw();
+}
+
 Board.prototype.inBounds = function(cell){
 	var m=cell.m, n=cell.n;
 	return m>=0 && m<=this.maxM && n>=0 && n<=(this.maxN-(m%2==0 ? 1 : 0));
@@ -17,15 +31,14 @@ Board.prototype.redraw = function(){
 	var ctx = this.context, tiles=this.tiles;
 	ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 	ctx.drawImage(tiles.border,0,0);
-	for ( var i=0,len=this.features.length; i<len; ++i ){
-		var cell = this.features[i];
-		ctx.drawImage( tiles[cell.tile], cell.x-HEX_WIDTH/2-.5, cell.y-HEX_HEIGHT/2 );
-	}
-	for ( var i=0,len=this.path.length; i<len; ++i ){
-		var cell = this.path[i];
-		ctx.drawImage( tiles[cell.tile], cell.x-HEX_WIDTH/2-.5, cell.y-HEX_HEIGHT/2 );
-	} 
+	for ( var i=0,len=this.features.length; i<len; ++i ) this.drawTile(this.features[i]);
+	for ( var i=0,len=this.path.length; i<len; ++i ) this.drawTile(this.path[i]);
+	this.drawTile(this.start);
+	this.drawTile(this.finish);
 	ctx.drawImage( tiles.hexgrid, 0, 0 );
+}
+Board.prototype.drawTile = function( cell ){
+	this.context.drawImage( this.tiles[cell.tile], cell.x-HEX_WIDTH/2-.5, cell.y-HEX_HEIGHT/2 );
 }
 
 Board.prototype.shortestPath = function(a,b){
